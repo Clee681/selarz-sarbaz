f729f0e6c2cfc5f83566b0d1c85250aea1e85909
Clee681 <christopher.lee681@gmail.com>
2014-01-10 12:13:25 -0800

    Add firm overview page. Abstract header and scripts partials.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
 	def index
 	end
+
+	def firm_overview
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ SelarzSarbaz::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get 'firm-overview' => 'static_pages#firm_overview', as: :overview
+
   root 'static_pages#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
